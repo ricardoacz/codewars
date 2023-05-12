efa55b318003fb70f215e37a9c74051bdfbaf24d
Ricardo Castillo <99503607+ricardoacz@users.noreply.github.com>
2023-05-12 07:32:42 -0400

    Create codewars-js-solution8.js

---
+++ codewars-js-solution8.js
@@ -0,0 +1,32 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+// Your function should only return a number, not the explanation about how you get that number.
+
+function getSum(a, b)
+{
+  const numbersBetween = []
+   if (a < b) {
+     for (let i = a; i <= b; i++) {
+       numbersBetween.push(i)
+     }
+     return numbersBetween.reduce( (sum, value) => (sum + value))
+   } else if (a > b) {
+     for (let i = b; i <= a; i++) {
+       numbersBetween.push(i)
+     }
+     return numbersBetween.reduce( (sum, value) => (sum + value))
+   } else if (a === b) {
+     return a
+   }
+  
+
+}
