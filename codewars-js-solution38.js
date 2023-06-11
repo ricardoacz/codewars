8ff7a86f718b50c0a1d63301c27ab8ccb0c859b6
Ricardo Castillo <99503607+ricardoacz@users.noreply.github.com>
2023-06-11 13:26:37 -0400

    Create codewars-js-solution38.js

---
+++ codewars-js-solution38.js
@@ -0,0 +1,27 @@
+// How good are you really?
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+function betterThanAverage(classPoints, yourPoints) {
+  classPoints.push(yourPoints)
+  
+  let average = 0
+  
+  classPoints.forEach((element, index) => (average += element))
+  
+  average = average / classPoints.length
+  
+  if (average < yourPoints) {
+    return true
+  } else {
+    return false
+  }
+}
