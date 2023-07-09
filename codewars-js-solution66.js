602a98f8b3f4f94887bdfb062832b521337ff454
Ricardo Castillo <99503607+ricardoacz@users.noreply.github.com>
2023-07-09 09:14:49 -0400

    Create codewars-js-solution66.js

---
+++ codewars-js-solution66.js
@@ -0,0 +1,17 @@
+// Is this a triangle?
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+function isTriangle(a,b,c)
+{
+   if (a > 0 && b > 0 && c > 0) {
+     if (a+b > c && b+c > a && a+c > b) {
+       return true
+     } else {
+       return false
+     }
+   } else {
+     return false
+   }
+}
