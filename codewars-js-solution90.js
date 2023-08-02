34746ddac4144dae0a97e13f0e22139441aca5c4
Ricardo Castillo <99503607+ricardoacz@users.noreply.github.com>
2023-08-02 13:19:39 -0400

    Create codewars-js-solution90.js

---
+++ codewars-js-solution90.js
@@ -0,0 +1,17 @@
+// 8 kyu
+// Remove duplicates from list
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+function distinct(a) {
+  let newArray = []
+  
+  for (let i = 0; i < a.length; i++) {
+    if (newArray.indexOf(a[i]) === -1) {
+      newArray.push(a[i])
+    }
+  }
+  
+  return newArray
+}
