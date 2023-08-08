c3255bb55d25b5af460b4f68dde867d273637eb5
Ricardo Castillo <99503607+ricardoacz@users.noreply.github.com>
2023-08-08 15:21:22 -0400

    Create codewars-js-solution96.js

---
+++ codewars-js-solution96.js
@@ -0,0 +1,23 @@
+// 7 kyu
+// Remove anchor from URL
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+  let arr = []
+  let urlArr = url.split("")
+  
+  for (let i = 0; i < urlArr.length; i++) {
+    if (urlArr[i] === '#') {
+      break
+    } else if (urlArr[i] !== '#') {
+      arr.push(urlArr[i])
+    }
+  }
+  
+  
+  return arr.join("")
+}
