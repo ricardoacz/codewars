9216398c3e0b074987af11da4c44c1f00ee0b47f
Ricardo Castillo <99503607+ricardoacz@users.noreply.github.com>
2023-08-28 11:42:37 -0400

    Create codewars-js-solution117.js

---
+++ codewars-js-solution117.js
@@ -0,0 +1,13 @@
+// 8 kyu
+// Printing Array elements with Comma delimiters
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+function printArray(array){
+  return array.join(",")
+}
