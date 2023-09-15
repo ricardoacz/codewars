0634226e540cb60cfea5e1ee1db8ff4460506941
Ricardo Castillo <99503607+ricardoacz@users.noreply.github.com>
2023-09-15 10:34:33 -0400

    Create codewars-js-solution135.js

---
+++ codewars-js-solution135.js
@@ -0,0 +1,7 @@
+// 8 kyu
+// Even or Odd
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+const evenOrOdd = (number) => {
+  return number % 2 === 0 ? 'Even' : 'Odd' 
+}
