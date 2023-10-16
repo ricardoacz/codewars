61880806b024712603be847b3f51b69c69ffb51a
Ricardo Castillo <99503607+ricardoacz@users.noreply.github.com>
2023-10-16 07:03:12 -0400

    Create codewars-js-solution167.js

---
+++ codewars-js-solution167.js
@@ -0,0 +1,22 @@
+// 8 kyu
+// Classy Classes
+// Classy Classes
+// Basic Classes, this kata is mainly aimed at the new JS ES6 Update introducing classes
+
+// Task
+// Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return johns age is 34
+
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+
+class Person {
+  constructor(name, age) {
+    this._name = name
+    this._age = age
+    this._info = `${this._name}s age is ${this._age}`
+  
+  }
+  
+ get info () {
+    return this._info
+  }
+}
