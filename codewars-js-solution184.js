e8d14e36f9c46fea1736f778543483931aea3652
Ricardo Castillo <99503607+ricardoacz@users.noreply.github.com>
2023-11-02 17:34:41 -0400

    Create codewars-js-solution184.js

---
+++ codewars-js-solution184.js
@@ -0,0 +1,11 @@
+// 8 kyu
+// Even or Odd
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    return 'Even'
+  } else if (number % 2 !== 0) {
+    return 'Odd'
+  }
+}
