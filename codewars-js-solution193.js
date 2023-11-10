ccdc9a468b6167d5328fee9cea755d88a64f0c5f
Ricardo Castillo <99503607+ricardoacz@users.noreply.github.com>
2023-11-09 21:54:11 -0500

    Create codewars-js-solution193.js

---
+++ codewars-js-solution193.js
@@ -0,0 +1,16 @@
+// 7 kyu
+// Consecutive letters
+function solve(s){
+    if (s.length === 1) {
+      return true
+    }
+  
+  s = s.split("").sort().join("")
+  
+  for (let i = 1; i < s.length; i++) {
+    if (s.charCodeAt(i) - s.charCodeAt(i - 1) !== 1) {
+      return false
+    }
+  }
+  return true
+}
