ce7e845ff0015b33e7622bbdd32dbf69869a3360
Ricardo Castillo <99503607+ricardoacz@users.noreply.github.com>
2023-11-25 12:46:22 -0500

    Create codewars-js-solution209.js

---
+++ codewars-js-solution209.js
@@ -0,0 +1,23 @@
+// 8 kyu
+// Is it a number?
+// Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+// Valid examples, should return true:
+
+// isDigit("3")
+// isDigit("  3  ")
+// isDigit("-3.23")
+// should return false:
+
+// isDigit("3-4")
+// isDigit("  3   5")
+// isDigit("3 5")
+// isDigit("zero")
+
+function isDigit(s) {
+  if (s == parseFloat(s)) {
+    return true
+  } else {
+    return false
+  }
+}
