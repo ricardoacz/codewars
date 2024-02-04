c009165a7ecf9ccbab845d41c64190cc07fd6d05
Ricardo Castillo <99503607+ricardoacz@users.noreply.github.com>
2024-02-04 10:09:59 -0500

    Create codewars-js-solution280.js

---
+++ codewars-js-solution280.js
@@ -0,0 +1,15 @@
+// 7 kyu
+// Likes Vs Dislikes
+
+function likeOrDislike(buttons) {
+  let result = 'Nothing'
+  
+  for (let i = 0; i < buttons.length; i++) {
+    if (result === buttons[i]) {
+      result = 'Nothing'
+    } else {
+      result = buttons[i]
+    }
+  }
+  return result
+}
